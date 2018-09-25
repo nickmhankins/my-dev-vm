e3ea02c4e8ef5c88432eec1375f9cc764ad7fc6e
Hankins <Nick.Hankins@nike.com>
2018-09-25 16:14:03 -0700

    working?

--- scripts/linux/vbguestadditions.sh
+++ scripts/linux/vbguestadditions.sh
@@ -1,10 +1,5 @@
 echo "==> VirtualBox Guest Additions"
-yum -y install gcc make bzip2 kernel-headers kernel-devel dkms "kernel-headers-$(uname -r)"
-#yum -y install "kernel-headers-$(uname -r)"
-#yum -y install "kernel-devel"
-#yum -y install "kernel-devel-$(uname -r)"
-# for some reason the vb guest additions needed this particular version below...
-#yum -y install "kernel-devel-3.10.0-862.11.6.el7"
+yum -y install gcc make bzip2 kernel-headers-$(uname -r) kernel-devel-$(uname -r)
 yum -y update
 mkdir /tmp/virtualbox
 VERSION=$(cat /home/vagrant/.vbox_version)
