5eb4db2854a3b31c485a64b39c4cf091b7f3683a
Hankins <Nick.Hankins@nike.com>
2018-10-08 12:18:44 -0700

    add path to bashrc

--- scripts/linux/terraform.sh
+++ scripts/linux/terraform.sh
@@ -3,4 +3,4 @@ yum install -y unzip
 curl -O https://releases.hashicorp.com/terraform/0.11.8/terraform_0.11.8_linux_amd64.zip
 unzip ./terraform_0.11.8_linux_amd64.zip -d /usr/local/bin
 rm -f ./terraform_0.11.8_linux_amd64.zip
-export PATH=$PATH:/usr/local/bin
+echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
