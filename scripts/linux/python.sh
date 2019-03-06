71634931c9ed6ebbf7700bd06f41937ad5b74d6f
nickmhankins <nickmhankins@gmail.com>
2019-03-06 02:32:21 +0000

    python version

--- scripts/linux/python.sh
+++ scripts/linux/python.sh
@@ -2,8 +2,7 @@
 yum install -y yum-utils
 yum groupinstall -y 'Development Tools'
 yum install -y https://centos7.iuscommunity.org/ius-release.rpm
-yum install -y python36u python36u-pip python36u-libs python36u-devel
-python3.6 -V
+yum install -y python36u python36u-pip python36u-libs python36u-devel python-pip
 alias python="python3.6" 
 alias pip="pip3.6"
-pip install azure
+#pip install azure
