02c4c93a9c95459bdd31a6327f58a791506e4f77
nhanki <nick.hankins@nike.com>
2019-06-26 11:14:46 -0700

    add zsh plugin automation, readme updates

--- puppet/modules/dotheneedful/manifests/ohmyzsh.pp
+++ puppet/modules/dotheneedful/manifests/ohmyzsh.pp
@@ -3,21 +3,29 @@ class dotheneedful::ohmyzsh {
   $user = 'vagrant'
   $base_uri = 'https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master'
   $installsource = "${base_uri}/tools/install.sh"
+  $repos = $dotheneedful::ohmyzsh_plugin_repos
 
   exec { 'install_ohmyzsh':
     command => "/usr/bin/curl -fsSL ${installsource} | /usr/bin/bash",
     user    => $user
   }
 
-  file { "/home/${user}/.zshrc":
-    ensure => 'file',
-    source => 'puppet:///modules/dotheneedful/.zshrc',
-    mode   => '0600',
-    owner  => 'vagrant'
-  }
-
   user { $user:
     shell  => '/bin/zsh'
+  }
+
+  $repos.each |String $repo| {
+    $name = split($repo, '/')[-1]
+    exec {"${repo}_install":
+      command => "/usr/bin/git clone ${repo} /home/${user}/.oh-my-zsh/custom/plugins/${name}"
+    }
+  }
+
+  file { "/home/${user}/.zshrc":
+    ensure  => 'file',
+    content => epp('dotheneedful/zshrc.epp'),
+    mode    => '0600',
+    owner   => $user
   }
 
 }
